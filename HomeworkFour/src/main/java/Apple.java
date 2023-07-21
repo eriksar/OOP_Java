0c23db5bf58d9c98b55160bfdb5078e5f3bb92cf
eriksar <123926096+eriksar@users.noreply.github.com>
2023-07-21 19:56:10 +0400

    Apple.java

---
+++ HomeworkFour/src/main/java/Apple.java
@@ -0,0 +1,12 @@
+public class Apple extends Fruit{
+
+    private int weight;
+    public Apple(int weight) {
+        super(weight);
+    }
+    @Override
+    public String toString() {
+        return "Apple " +
+                "weight " + getWeight();
+    }
+}
